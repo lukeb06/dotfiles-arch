a1872687c431e05d71f7511f77e44a092ed91133
Luke <lukebbarrier06@outlook.com>
2025-12-01 06:28:42 -0500

    Install Oh My Zsh in ci.sh
    Added command to install Oh My Zsh during setup.

--- ci.sh
+++ ci.sh
@@ -3,6 +3,7 @@
 sudo chmod +x ./scripts/pkg-install.sh
 sudo chmod +x ./scripts/pkg-dump.sh
 sudo ./scripts/pkg-install.sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ./scripts/install-hooks.sh
 ./scripts/stow.sh
 
