c1c837e4e3ef2bd2c2f00f3ef2f7b8d3bf4ca844
Luke <lukebbarrier06@outlook.com>
2025-12-01 07:38:59 -0500

    Update path.zsh

--- .zshconfig/path.zsh
+++ .zshconfig/path.zsh
@@ -1,4 +1,4 @@
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 export NVM_DIR="/home/luke/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
@@ -13,12 +13,13 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 # tmuxifier
 export PATH="$HOME/.tmuxifier/bin:$PATH"
 
-export DYLD_FALLBACK_LIBRARY_PATH="$(brew --prefix)/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+# export DYLD_FALLBACK_LIBRARY_PATH="$(brew --prefix)/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
 # Shopify Hydrogen alias to local projects
 alias h2='$(npm prefix -s)/node_modules/.bin/shopify hydrogen'
 
-export EDITOR="/opt/homebrew/bin/nvim"
+# export EDITOR="/opt/homebrew/bin/nvim"
+export EDITOR="$(which nvim)"
 export VISUAL="$EDITOR"
 
 function _gi() { curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/$@ ;}
