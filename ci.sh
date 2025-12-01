696e10c5dafd657d7316a9426edec506bb3c3967
Luke <lukebbarrier06@outlook.com>
2025-12-01 07:14:50 -0500

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,9 +3,7 @@
 sudo chmod +x ./scripts/pkg-install.sh
 sudo chmod +x ./scripts/pkg-dump.sh
 ./scripts/pkg-install.sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ./scripts/install-hooks.sh
 rm -rf ~/.config/hypr
 ./scripts/stow.sh
-
-zsh -c "echo 'Installation complete!'"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
