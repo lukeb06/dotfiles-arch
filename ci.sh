be1bc96c9d37d841d28be628b32b571f5e1fc294
Luke <lukebbarrier06@outlook.com>
2025-12-01 06:45:34 -0500

    Fix execution of pkg-install.sh script

--- ci.sh
+++ ci.sh
@@ -2,7 +2,7 @@
 
 sudo chmod +x ./scripts/pkg-install.sh
 sudo chmod +x ./scripts/pkg-dump.sh
-sudo ./scripts/pkg-install.sh
+./scripts/pkg-install.sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ./scripts/install-hooks.sh
 rm -rf ~/.config/hypr
