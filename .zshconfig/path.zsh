27b3d1dea370a726ce143fd7978f57c1be4d5f54
Luke <lukebbarrier06@outlook.com>
2025-12-02 20:55:41 -0500

    Update path.zsh

--- .zshconfig/path.zsh
+++ .zshconfig/path.zsh
@@ -3,22 +3,11 @@
 export NVM_DIR="/home/$(whoami)/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# bun completions
-[ -s "/home/$(whoami)/.bun/_bun" ] && source "/home/$(whoami)/.bun/_bun"
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
-
 # tmuxifier
 export PATH="$HOME/.tmuxifier/bin:$PATH"
 
 # export DYLD_FALLBACK_LIBRARY_PATH="$(brew --prefix)/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 
-# Shopify Hydrogen alias to local projects
-alias h2='$(npm prefix -s)/node_modules/.bin/shopify hydrogen'
-
-# export EDITOR="/opt/homebrew/bin/nvim"
 export EDITOR="$(which nvim)"
 export VISUAL="$EDITOR"
 
